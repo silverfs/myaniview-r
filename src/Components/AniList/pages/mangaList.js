55f8daedb4b992b9818b5c1ea9afe559d6b4e484
SilverFS <faizalsusetyo@gmail.com>
2022-05-18 23:37:11 +0200

    Added context + loading gif

--- src/Components/AniList/pages/mangaList.js
+++ src/Components/AniList/pages/mangaList.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useMangaList } from '../Hooks/useMangaList';
+import second from '../../../img/loading.gif'
 
 export default function MangaList() {
   const {error, loading, data } = useMangaList();
-  if (loading) return <div>LOADING...</div>;
+  if (loading) return <img src={second} alt='loadinggif'></img>;
   if (error) return <div>Whoops! Something went wrong...</div>
 
   return <div className="MangaList">
     {data.Page.media.map(manga => {
       return <div key={manga.id}>        
-          <h2>{manga.title.romaji}</h2>
+          <h2><b>English: </b>{manga.title.romaji}</h2>
           <img src={manga.coverImage.medium} alt='Cover' />
         </div>
     })}
