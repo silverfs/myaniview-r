d4399c6ac1703b2126ed47bb577586e636d24cf0
SilverFS <faizalsusetyo@gmail.com>
2022-05-25 12:50:15 +0200

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react'
 import { Route, Routes } from 'react-router'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav';
 import Manga from './Components/AniList/pages/manga';
 import Profile from './Components/AniList/pages/profile'
 import MangaList from './Components/AniList/pages/mangaList'
@@ -10,6 +12,16 @@ import NotFound from './Components/AniList/pages/notFound';
 function App() {
   return (
     <div className="App">
+      <Navbar className='navbg' variant='dark' fixed='top' expand='md'>
+        <Navbar.Brand href='/'>MAV</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav>
+            <Nav.Link href='/'>AniList</Nav.Link>
+            <Nav.Link href='/'>Coming Soon</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <Routes>
         <Route strict exact path='/' element={<Profile />} />
         <Route strict exact path='/manga' element={<MangaList />} />
